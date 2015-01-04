51e985bb74f1db96aa955d6dde4cc572fd1c8da1
Michael Kirschner <mjk.kirschner@gmail.com>
2015-01-03 19:40:37 -0500

    remove two tests that were used when trying to find ASM 220 issues

--- src/UnfoldTests/GeometryTests.cs
+++ src/UnfoldTests/GeometryTests.cs
@@ -8,124 +8,98 @@ using Unfold.Topology;
 
 namespace UnfoldTests
 {
-    public class GeometryTests
-    {
-        [Test]
-        public void CanWrapFacesFromCube()
-        {
+	public class GeometryTests
+	{
+		[Test]
+		public void CanWrapFacesFromCube()
+		{
 
-            using (Solid testcube = UnfoldTestUtils.SetupCube())
-            {
+			using (Solid testcube = UnfoldTestUtils.SetupCube())
+			{
 
-                var faces = testcube.Faces.ToList();
+				var faces = testcube.Faces.ToList();
 
-                var wrappedFaces = faces.Select(x => new FaceLikeEntity(x)).ToList();
-                foreach (IDisposable item in wrappedFaces)
-                {
-                    Console.WriteLine("disposing a wrapped face");
-                    item.Dispose();
-                }
-
-
-            }
-        }
-
-        [Test]
-        public void CanWrapSurfacesFromCube()
-        {
-
-            Solid testcube = UnfoldTestUtils.SetupCube();
-            var faces = testcube.Faces.ToList();
-            var surfaces = faces.Select(x => x.SurfaceGeometry()).ToList();
-            testcube.Dispose();
-            var wrappedFaces = surfaces.Select(x => new FaceLikeEntity(x)).ToList();
-            foreach (IDisposable item in wrappedFaces)
-            {
-                Console.WriteLine("disposing a wrapped face");
-                item.Dispose();
-            }
-            foreach (IDisposable item in faces)
-            {
-                Console.WriteLine("disposing a face");
-                item.Dispose();
-            }
+				var wrappedFaces = faces.Select(x => new FaceLikeEntity(x)).ToList();
+				foreach (IDisposable item in wrappedFaces)
+				{
+					Console.WriteLine("disposing a wrapped face");
+					item.Dispose();
+				}
 
 
-        }
+			}
+		}
+
+		[Test]
+		public void CanWrapSurfacesFromCube()
+		{
+
+			Solid testcube = UnfoldTestUtils.SetupCube();
+			var faces = testcube.Faces.ToList();
+			var surfaces = faces.Select(x => x.SurfaceGeometry()).ToList();
+			testcube.Dispose();
+			var wrappedFaces = surfaces.Select(x => new FaceLikeEntity(x)).ToList();
+			foreach (IDisposable item in wrappedFaces)
+			{
+				Console.WriteLine("disposing a wrapped face");
+				item.Dispose();
+			}
+			foreach (IDisposable item in faces)
+			{
+				Console.WriteLine("disposing a face");
+				item.Dispose();
+			}
+
+
+		}
 
 
 
-        [Test]
-        public void CanWrapFacesFrom10000Cubes()
-        {
-            foreach (var i in Enumerable.Range(0, 10000))
-            {
-                Console.WriteLine(i);
-                CanWrapFacesFromCube();
+		[Test]
+		public void CanWrapFacesFrom10000Cubes()
+		{
+			foreach (var i in Enumerable.Range(0, 10000))
+			{
+				Console.WriteLine(i);
+				CanWrapFacesFromCube();
 
-            }
-        }
+			}
+		}
 
-        [Test]
-        public void CanWrapSurfacesFrom10000Cubes()
-        {
-            foreach (var i in Enumerable.Range(0, 10000))
-            {
-                Console.WriteLine(i);
-                CanWrapSurfacesFromCube();
+		[Test]
+		public void CanWrapSurfacesFrom10000Cubes()
+		{
+			foreach (var i in Enumerable.Range(0, 10000))
+			{
+				Console.WriteLine(i);
+				CanWrapSurfacesFromCube();
 
-            }
-        }
+			}
+		}
 
-        [Test]
-        public void TestBasicGeometry()
-        {
-            var p1 = Point.ByCoordinates(0, 0, 0);
-            var p2 = Point.ByCoordinates(10, 0, 0);
+		[Test]
+		public void TestBasicGeometry()
+		{
+			var p1 = Point.ByCoordinates(0, 0, 0);
+			var p2 = Point.ByCoordinates(10, 0, 0);
 
-            var p3 = Point.ByCoordinates(0, 10, 0);
-            var p4 = Point.ByCoordinates(10, 10, 0);
+			var p3 = Point.ByCoordinates(0, 10, 0);
+			var p4 = Point.ByCoordinates(10, 10, 0);
 
-            var l1 = Line.ByStartPointEndPoint(p1, p2);
-            var l2 = Line.ByStartPointEndPoint(p3, p4);
+			var l1 = Line.ByStartPointEndPoint(p1, p2);
+			var l2 = Line.ByStartPointEndPoint(p3, p4);
 
-            var crvs = new Curve[] { l1, l2 };
+			var crvs = new Curve[] { l1, l2 };
 
-            var surf = Surface.ByLoft(crvs);
+			var surf = Surface.ByLoft(crvs);
 
-            Assert.IsNotNull(surf);
+			Assert.IsNotNull(surf);
 
-            Console.WriteLine(surf.Area);
-        }
-
-
-        [Test]
-        public void Creating10000CubesExtractFaces()
-        {
-            foreach (var i in Enumerable.Range(0, 10000))
-            {
-                Console.WriteLine(i);
-                Solid testcube = UnfoldTestUtils.SetupCube();
-                //testcube.Dispose();
-                //List<Face> faces = testcube.Faces.ToList();
-                GC.Collect();
-                GC.WaitForPendingFinalizers();
-            }
-        }
+			Console.WriteLine(surf.Area);
+		}
 
 
-        [Test]
-        public void Creating10000Cubes()
-        {
-            List<Solid> retainedlistofcubes = new List<Solid>();
-            foreach (var i in Enumerable.Range(0, 10000))
-            {
-                Console.WriteLine(i);
-                Solid testcube = UnfoldTestUtils.SetupCube();
-                //retainedlistofcubes.Add(testcube);
 
-            }
-        }
 
-    }
+	}
 }
